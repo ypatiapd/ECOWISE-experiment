ea2dc2a0df769815871fb3ae8f7c9ba6ecb55319
Ypatia Dami <ypatiapd@gmail.com>
2021-09-11 21:42:49 +0300

    Update adc.py

--- adc.py
+++ adc.py
@@ -53,31 +53,3 @@ class adc:
 
 
 
-# calibration proceedure just in case needed
-"""while True:
-                # Read all the ADC channel values in a list.
-                values = [0] * 4
-                for i in range(4):
-                    # Read the specified ADC channel using the previously set gain value.
-                    values[i] = self.adc.read_adc(i, gain=self.GAIN)
-                    values[i] = values[i] * 1.724 / 13785
-
-                # Note you can also pass in an optional data_rate parameter that controls
-                # the ADC conversion time (in samples/second). Each chip has a different
-                # set of allowed data rate values, see datasheet Table 9 config register
-                # DR bit values.
-                # values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
-                # Each value will be a 12 or 16 bit signed integer value depending on the
-                # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
-                # Print the ADC values.
-
-                # R=100.2*(values[2]-values[3])/(3.3-(values[2]-values[3]))
-                # temp_anal=(R-101)/0.385
-                # temp_anal = (333.3 - 201.2 * (values[2] - values[3])) / (0.385 * (values[2] - values[3]) - 1.2705)
-
-                temp_anal = 355.522 * values[3] - 608.232
-                print("temp_analllll=" + format(temp_anal))
-                print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
-                # Pause for half a second.
-                # temp = temp_sensor.get_ambient()
-                # print("tempMLX="+format(temp))"""
