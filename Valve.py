0866f1dd330f2292b915a3f4357d8196c3bd280e
Ypatia Dami <ypatiapd@gmail.com>
2021-09-11 21:42:02 +0300

    Update Valve.py

--- Valve.py
+++ Valve.py
@@ -10,7 +10,6 @@ class valve:
         if self.valve_number == 1:
             self.solenoid_pin = 8
         elif self.valve_number == 2:
-            #self.solenoid_pin = 25
             self.solenoid_pin = 7
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.solenoid_pin, GPIO.OUT)
@@ -28,5 +27,6 @@ class valve:
 
         GPIO.output(self.solenoid_pin, GPIO.LOW)
         self.exp_info_logger.write_info("valve" + format(self.valve_number) + "closed")
+
 
 
